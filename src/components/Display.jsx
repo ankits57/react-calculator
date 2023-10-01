0f5b133bf26cbe10b6bf97e749e45eee6b981fc5
Ankit Saurabh <ankitsaurabh57@gmail.com>
2023-10-01 21:18:20 +0530

    Optimization

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Display = ({text}) => {
   return (
-    <div className='relative bg-black h-96 lg:h-screen'>
+    <div className='relative bg-black h-[280px] lg:h-screen'>
         <div className='absolute bottom-0 right-0 h-30 w-full mt-auto pr-1'>
             <h1 className='text-white text-end text-8xl tracking-tighter overflow-hidden break-words'>{text}</h1>
         </div>
