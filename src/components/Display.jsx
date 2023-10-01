06c7ee27978931e08baf3a260b3b3290b3807247
Ankit Saurabh <ankitsaurabh57@gmail.com>
2023-10-01 21:36:38 +0530

    Theme UI

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {BsThreeDotsVertical} from 'react-icons/bs'
 
 const Display = ({text}) => {
   return (
     <div className='relative bg-black h-[280px] lg:h-screen'>
+        <button className='text-white w-8 h-8 text-xl' type='button'><BsThreeDotsVertical/></button>
         <div className='absolute bottom-0 right-0 h-30 w-full mt-auto pr-1'>
             <h1 className='text-white text-end text-8xl tracking-tighter overflow-hidden break-words'>{text}</h1>
         </div>
